bf7f69b55bde8f4552e200e2ce244a8194d379f1
renjani2022 <rinjani.ps@gmail.com>
2022-10-07 20:34:00 +0200

    solving exercises

--- 2-control-flow/1-isolate/1-conditional-statements/exercises/2-initial-values/3.js
+++ 2-control-flow/1-isolate/1-conditional-statements/exercises/2-initial-values/3.js
@@ -8,8 +8,8 @@ console.log('-- begin --');
 //  what do all solutions to the same path have in common?
 // or maybe there are unreachable paths!
 
-const value1 = _;
-const value2 = _;
+const value1 = false;
+const value2 = true;
 let path = '';
 
 if (value1 && !value2) {
